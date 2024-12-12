7b8742278c083a82b0144044ca21b8ee03955c48
soumeh <soumeh@proton.me>
2024-12-12 19:57:30 +0100

    remove deno components

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,5 @@
 import { defineConfig } from "astro/config";
-import deno from "@deno/astro-adapter";
 
-// https://astro.build/config
 export default defineConfig({
 	site: "https://sindercu.be",
-	output: "server",
-	adapter: deno(),
 });
